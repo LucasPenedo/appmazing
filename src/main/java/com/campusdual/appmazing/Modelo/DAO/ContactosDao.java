341a9a528202e2345cbb61f7086c86fc094f6d3a
LucasPenedo <lucaspenedo9@gmail.com>
2023-09-26 12:12:46 +0200

    Añadir DAO de productos y contactos

---
+++ src/main/java/com/campusdual/appmazing/Modelo/DAO/ContactosDao.java
@@ -0,0 +1,8 @@
+package com.campusdual.appmazing.Modelo.DAO;
+
+import com.campusdual.appmazing.Modelo.Contactos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactosDao extends JpaRepository<Contactos,Integer> {
+
+}
---
+++ src/main/java/com/campusdual/appmazing/Modelo/DAO/ProductDao.java
@@ -0,0 +1,11 @@
+package com.campusdual.appmazing.Modelo.DAO;
+
+import com.campusdual.appmazing.Modelo.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductDao extends JpaRepository<Product,Integer> {
+
+
+
+
+}
