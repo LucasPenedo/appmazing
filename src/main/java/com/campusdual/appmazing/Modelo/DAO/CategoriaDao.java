a40786ae6cbf739721590fe983d7831872d7f440
Lucas <lucaspenedo9@gmail.com>
2023-11-06 14:37:59 +0100

    Nueva Backend

--- src/main/java/com/campusdual/appmazing/Modelo/DAO/CategoriaDao.java
+++
@@ -1,6 +0,0 @@
-package com.campusdual.appmazing.Modelo.DAO;
-import com.campusdual.appmazing.Modelo.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaDao extends JpaRepository<Categoria, Integer>{
-}
--- src/main/java/com/campusdual/appmazing/Modelo/DAO/ContactosDao.java
+++
@@ -1,8 +0,0 @@
-package com.campusdual.appmazing.Modelo.DAO;
-
-import com.campusdual.appmazing.Modelo.Contactos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactosDao extends JpaRepository<Contactos,Integer> {
-
-}
